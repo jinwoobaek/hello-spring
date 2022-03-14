25582fff13b2355c0ac9d8dbfa07c54905271c9b
Jinwoo Baek <back3946@gmail.com>
2022-03-14 16:26:25 +0900

    자바 코드로 빈 등록하기

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,26 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 컴포넌트 스캔 방식 말고,
+ * 자바 코드로 빈 등록하기
+ * */
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
